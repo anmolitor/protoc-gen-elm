da5279ee50228175d194440a04246885febd84cb
aewering <andreas.ewering@andrena.de>
2021-11-12 11:34:51 +0100

    update uglifyjs

--- build.sh
+++ build.sh
@@ -3,5 +3,5 @@
 set -e
 
 elm make --optimize --output=elm.tmp.js src/Main.elm
-uglifyjs elm.tmp.js --compress 'pure_funcs="F2,F3,F4,F5,F6,F7,F8,F9,A2,A3,A4,A5,A6,A7,A8,A9",pure_getters,keep_fargs=false,unsafe_comps,unsafe' | node_modules/uglify-js/bin/uglifyjs --mangle --output=elm.min.js
+uglifyjs elm.tmp.js --compress 'pure_funcs="F2,F3,F4,F5,F6,F7,F8,F9,A2,A3,A4,A5,A6,A7,A8,A9",pure_getters,keep_fargs=false,unsafe_comps,unsafe' | node_modules/uglify-js/bin/uglifyjs --mangle --output elm.min.js
 rm elm.tmp.js
