dc94d3499196f299a95c9dab3a981cb3545fd173
aewering <andreas.ewering@andrena.de>
2023-04-07 17:59:43 +0200

    complete very basic todo app

--- example/tonic_vite/src/Main.elm
+++ example/tonic_vite/src/Main.elm
@@ -3,15 +3,17 @@ module Main exposing (main)
 import Browser
 import Browser.Navigation
 import Grpc
-import Html exposing (Html, button, div, form, input, text)
-import Html.Attributes
+import Html exposing (Html, button, div, form, h2, input, label, p, text, textarea)
+import Html.Attributes exposing (class)
 import Html.Events exposing (onClick, onInput, onSubmit)
+import Iso8601
 import Ports
 import Proto.Google.Protobuf
 import Proto.Todos exposing (Todo, TodoWithId)
 import Proto.Todos.TodoService
 import Protobuf.Types.Int64 exposing (Int64)
 import Random
+import Time
 import UUID
 
 
@@ -29,27 +31,45 @@ type Msg
     | CreateRandomUser
     | CreatedRandomUser UUID.UUID
     | Logout
-    | AddTodo UserId Todo
-    | AddedTodo (Result Grpc.Error TodoId)
-    | DeleteTodo TodoId
-    | DeletedTodo (Result Grpc.Error TodoId)
     | GetTodos UserId
     | GotTodos (Result Grpc.Error (List TodoWithId))
+    | SetNewTodo TodoForm
+    | AddTodo UserId Todo
+    | AddedTodo (Result Grpc.Error TodoWithId)
+    | DeleteTodo TodoId
+    | DeletedTodo (Result Grpc.Error TodoId)
+    | FormValidationError String
 
 
 type alias Model =
-    { userId : Maybe String, loginFormFieldValue : String, todos : List TodoWithId }
+    { userId : Maybe String
+    , loginFormFieldValue : String
+    , todos : List TodoWithId
+    , todoForm : TodoForm
+    }
+
+
+type alias TodoForm =
+    { title : String
+    , description : String
+    , dueAt : String
+    }
+
+
+initTodoForm : Time.Posix -> TodoForm
+initTodoForm dueAt =
+    { title = "", description = "", dueAt = Iso8601.fromTime dueAt }
 
 
 type alias Flags =
-    { userId : Maybe String }
+    { userId : Maybe String, now : Int }
 
 
 init : Flags -> ( Model, Cmd Msg )
 init flags =
     let
         model =
-            { userId = Nothing, loginFormFieldValue = "", todos = [] }
+            { userId = Nothing, loginFormFieldValue = "", todos = [], todoForm = initTodoForm <| Time.millisToPosix flags.now }
     in
     case flags.userId of
         Just userId ->
@@ -93,8 +113,11 @@ update msg model =
             ( model
             , { userId = userId, todo = Just todo }
                 |> Grpc.new Proto.Todos.TodoService.addTodo
-                |> Grpc.toCmd (Result.map .todoId >> AddedTodo)
+                |> Grpc.toCmd (Result.map (\{ todoId } -> { id = todoId, todo = Just todo }) >> AddedTodo)
             )
+
+        AddedTodo (Ok todoWithId) ->
+            ( { model | todos = todoWithId :: model.todos }, Cmd.none )
 
         DeleteTodo todoId ->
             ( model
@@ -102,6 +125,12 @@ update msg model =
                 |> Grpc.new Proto.Todos.TodoService.deleteTodo
                 |> Grpc.toCmd (Result.map (always todoId) >> DeletedTodo)
             )
+
+        DeletedTodo (Ok todoId) ->
+            ( { model | todos = List.filter (\todo -> todo.id /= todoId) model.todos }, Cmd.none )
+
+        SetNewTodo todoForm ->
+            ( { model | todoForm = todoForm }, Cmd.none )
 
         _ ->
             ( model, Cmd.none )
@@ -126,19 +155,72 @@ subscriptions _ =
     Sub.none
 
 
+posixToTimestamp : Time.Posix -> Proto.Google.Protobuf.Timestamp
+posixToTimestamp posix =
+    let
+        millis =
+            Time.posixToMillis posix
+
+        seconds =
+            millis // 1000
+
+        nanos =
+            modBy 1000 millis
+    in
+    { seconds = Protobuf.Types.Int64.fromInts 0 seconds, nanos = nanos }
+
+
+submitForm : UserId -> TodoForm -> Msg
+submitForm userId todoForm =
+    case Iso8601.toTime todoForm.dueAt of
+        Ok dueAt ->
+            AddTodo userId
+                { title = todoForm.title, description = todoForm.description, dueAt = Just <| posixToTimestamp dueAt }
+
+        Err _ ->
+            FormValidationError "Invalid date"
+
+
 view : Model -> Browser.Document Msg
-view model =
+view ({ todoForm } as model) =
     case model.userId of
         Just userId ->
             { title = "Todo App"
             , body =
                 [ button [ onClick Logout ] [ text "Logout" ]
-                , button
-                    [ onClick <|
-                        AddTodo userId
-                            { title = "First todo", description = "", dueAt = Just Proto.Google.Protobuf.defaultTimestamp }
+                , form
+                    [ class "todo-form"
+                    , onSubmit <| submitForm userId todoForm
                     ]
-                    [ text "Add todo" ]
+                    [ label []
+                        [ text "Title"
+                        , input
+                            [ Html.Attributes.value model.todoForm.title
+                            , onInput <| \newTitle -> SetNewTodo { todoForm | title = newTitle }
+                            ]
+                            []
+                        ]
+                    , label []
+                        [ text "Description"
+                        , textarea
+                            [ Html.Attributes.value model.todoForm.description
+                            , onInput <| \newDescription -> SetNewTodo { todoForm | description = newDescription }
+                            ]
+                            []
+                        ]
+                    , label []
+                        [ text "Due at"
+                        , input
+                            [ Html.Attributes.type_ "datetime-local"
+                            , Html.Attributes.value todoForm.dueAt
+                            , onInput <| \newDueAt -> SetNewTodo { todoForm | dueAt = newDueAt }
+                            ]
+                            []
+                        ]
+                    , button
+                        []
+                        [ text "Add todo" ]
+                    ]
                 , div [] <| List.map viewTodo model.todos
                 ]
             }
@@ -150,8 +232,14 @@ view model =
 
 
 viewTodo : TodoWithId -> Html Msg
-viewTodo todo =
-    text (todo.todo |> Maybe.map .title |> Maybe.withDefault "")
+viewTodo { todo, id } =
+    case todo of
+        Just { title, description, dueAt } ->
+            div []
+                [ h2 [] [ text title ], p [] [ text description ], button [ onClick <| DeleteTodo id ] [ text "Remove" ] ]
+
+        Nothing ->
+            text ""
 
 
 loginForm : Model -> List (Html Msg)
