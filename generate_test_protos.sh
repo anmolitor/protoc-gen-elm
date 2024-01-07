bb3ec0e9b71fba37b1b84489be584b610f007cbc
aewering <andreas.ewering@andrena.de>
2024-01-07 16:45:43 +0100

    test json code generation

--- generate_test_protos.sh
+++ generate_test_protos.sh
@@ -6,4 +6,4 @@ files=$(find tests/proto -type f -printf "%p ")
 
 rm -rf generated
 mkdir generated
-protoc --proto_path=tests/proto --elm_out=generated --plugin="protoc-gen-elm=index.js" $files
+protoc --proto_path=tests/proto --elm_out=generated --elm_opt=json=all --plugin="protoc-gen-elm=index.js" $files
